9c1d8be979f6e9d3f657428a8c20646fb17d85c6
dvimukthi <deshanvimukthi59@gmail.com>
2023-03-12 20:25:40 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
-  Contacts,
+  Contact,
   Experience,
   Feedbacks,
   Hero,
@@ -14,10 +14,19 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-10 bg-primary">
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
